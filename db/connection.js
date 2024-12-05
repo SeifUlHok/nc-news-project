3eaef2115b4edb8c50408b091bbd18acc3f7bd7f
SeifUlHok <seifulhok@gmail.com>
2024-12-05 16:43:38 +0000

    add hosting changes

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,6 @@ if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
-module.exports = new Pool(config);
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -18,4 +17,4 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
