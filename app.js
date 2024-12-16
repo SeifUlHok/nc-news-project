4edb103f0256de9f7705681d607f8ed29ed52823
SeifUlHok <seifulhok@gmail.com>
2024-12-16 09:50:38 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {getEndpoints, getAllTopics, getArticleById, getAllArticles, getCommentsByArticle, postCommentByArticle, patchArticle, deleteComment, getUsers} = require("./controller");
 const app = express()
+const cors = require('cors');
+app.use(cors());
 app.use(express.json())
 
 app.get('/api', getEndpoints)
