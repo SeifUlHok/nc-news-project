f45c7d76cab97185947d99eda9e3927f222acb14
SeifUlHok <seifulhok@gmail.com>
2025-01-05 20:21:00 +0000

    updated delete comment

--- models.js
+++ models.js
@@ -102,7 +102,11 @@ function updateVotes(votes,id){
 
 function deleteCommentSql(comment_id){
     return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id]).then(({rows}) =>{
-        return rows;
+        if(rows.length === 0){
+            return Promise.reject({status:  404, msg: "comment not found"})
+        }else{
+            return rows;
+        }
     })
 };
 
