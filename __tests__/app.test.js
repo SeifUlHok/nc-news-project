c55b5c0b26bec2b328313d2ff17b15c4fb94661a
SeifUlHok <seifulhok@gmail.com>
2025-01-05 17:00:11 +0000

    c

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -162,11 +162,12 @@ describe("GET /api/articles/:article_id/comments", () => {
       });
   });
 
-  test("200: return empty array when no comments for article", () => {
+  test.only("200: return empty array when no comments for article", () => {
     return request(app)
       .get("/api/articles/37/comments")
       .expect(200)
       .then(({ body: { comments } }) => {
+        console.log(comments)
         expect(comments).toEqual(expect.objectContaining([]));
       });
   });
